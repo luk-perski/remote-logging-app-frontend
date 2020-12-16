e419dc3c7aefa1bc5f1833c2b4c33599073ddff7
Lukasz <lpizu@iscte-iul.pt>
2020-12-16 18:32:30 +0000

    Remove import

--- app/models/db/remote/logging/Team.java
+++ app/models/db/remote/logging/Team.java
@@ -2,7 +2,6 @@ package models.db.remote.logging;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.ebean.Model;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import models.db.user.User;
